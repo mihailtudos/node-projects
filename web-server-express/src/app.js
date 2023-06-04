7473dcd604605b27cb42fa83a09e73deaa6b8d9a
Mihail Tudos <mihail.tudos@idwalmarine.com>
2023-06-04 12:41:53 +0100

    added template engine hbs

--- web-server-express/src/app.js
+++ web-server-express/src/app.js
@@ -7,6 +7,23 @@ const publicPath = express.static(path.join(__dirname, '../public'));
 
 const app = express();
 app.use(publicPath);
+app.set('view engine', 'hbs');
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: "Weather app",
+        name: "Mihail Tudos"
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render("about");
+});
+
+app.get('/help', (req, res) => {
+    res.render("help");
+});
+
 
 app.get('/weather', (req, res) => {
     res.send("weater page");
